12f9486d94a687dbb3d2f2341201e88f4902df15
maksyuki <maksyuki@126.com>
2025-11-26 07:46:34 +0800

    feat: use pack struct to rewrite the module

--- rtl/archinfo_reg_pkg.sv
+++ rtl/archinfo_reg_pkg.sv
@@ -8,7 +8,7 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-module archinfo_reg_pkg;
+package archinfo_reg_pkg;
 
   typedef struct packed {
     logic [11:0] clock;
@@ -22,12 +22,22 @@ module archinfo_reg_pkg;
     logic [5:0]  cust;
   } archinfo_idl_reg_t;
 
-  typedef struct packed {
-    logic [23:0] date;
-  } archinfo_idh_reg_t;
+  typedef struct packed {logic [23:0] date;} archinfo_idh_reg_t;
 
   parameter logic [3:0] ARCHINFO_SYS = 4'b0000;  // BASEADDR + 0x00
   parameter logic [3:0] ARCHINFO_IDL = 4'b0001;  // BASEADDR + 0x04
   parameter logic [3:0] ARCHINFO_IDH = 4'b0010;  // BASEADDR + 0x08
 
-endmodule
+  parameter logic [31:0] ARCHINFO_SYS_ADDR = {26'b0, ARCHINFO_SYS, 2'b00};
+  parameter logic [31:0] ARCHINFO_IDL_ADDR = {26'b0, ARCHINFO_IDL, 2'b00};
+  parameter logic [31:0] ARCHINFO_IDH_ADDR = {26'b0, ARCHINFO_IDH, 2'b00};
+
+  parameter ARCHINFO_SYS_WIDTH = 20;
+  parameter ARCHINFO_IDL_WIDTH = 32;
+  parameter ARCHINFO_IDH_WIDTH = 24;
+
+  parameter archinfo_sys_reg_t ARCHINFO_SYS_INIT = 20'hF_1010;
+  parameter archinfo_idl_reg_t ARCHINFO_IDL_INIT = 32'hFFFF_2022;
+  parameter archinfo_idh_reg_t ARCHINFO_IDH_INIT = 24'hFF_FFFF;
+
+endpackage
