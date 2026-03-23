d65dd13c83a1176668bf5178004814a1551bf6ba
maksyuki <maksyuki@126.com>
2026-03-23 17:32:26 +0800

    feat: replace 'parameter' with 'localparam' for pkg value

--- rtl/archinfo_reg_pkg.sv
+++ rtl/archinfo_reg_pkg.sv
@@ -24,16 +24,16 @@ package archinfo_reg_pkg;
 
   typedef struct packed {logic [23:0] date;} archinfo_idh_reg_t;
 
-  parameter logic [3:0] ARCHINFO_SYS = 4'b0000;
-  parameter logic [3:0] ARCHINFO_IDL = 4'b0001;
-  parameter logic [3:0] ARCHINFO_IDH = 4'b0010;
+  localparam logic [3:0] ARCHINFO_SYS = 4'b0000;
+  localparam logic [3:0] ARCHINFO_IDL = 4'b0001;
+  localparam logic [3:0] ARCHINFO_IDH = 4'b0010;
 
-  parameter logic [31:0] ARCHINFO_SYS_ADDR = {26'b0, ARCHINFO_SYS, 2'b00};
-  parameter logic [31:0] ARCHINFO_IDL_ADDR = {26'b0, ARCHINFO_IDL, 2'b00};
-  parameter logic [31:0] ARCHINFO_IDH_ADDR = {26'b0, ARCHINFO_IDH, 2'b00};
+  localparam logic [31:0] ARCHINFO_SYS_ADDR = {26'b0, ARCHINFO_SYS, 2'b00};
+  localparam logic [31:0] ARCHINFO_IDL_ADDR = {26'b0, ARCHINFO_IDL, 2'b00};
+  localparam logic [31:0] ARCHINFO_IDH_ADDR = {26'b0, ARCHINFO_IDH, 2'b00};
 
-  parameter archinfo_sys_reg_t ARCHINFO_SYS_INIT = 20'hF_1010;
-  parameter archinfo_idl_reg_t ARCHINFO_IDL_INIT = 32'hFFFF_2022;
-  parameter archinfo_idh_reg_t ARCHINFO_IDH_INIT = 24'hFF_FFFF;
+  localparam archinfo_sys_reg_t ARCHINFO_SYS_INIT = 20'hF_1010;
+  localparam archinfo_idl_reg_t ARCHINFO_IDL_INIT = 32'hFFFF_2022;
+  localparam archinfo_idh_reg_t ARCHINFO_IDH_INIT = 24'hFF_FFFF;
 
 endpackage
